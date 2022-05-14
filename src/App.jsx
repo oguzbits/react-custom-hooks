be5221aba4d1db3788c4f44c724fde13a2a325a1
Oguz <oguz.oeztuerk@outlook.de>
2022-05-14 17:47:07 +0200

    add useTimeout custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
-import ToggleComponent from './useToggle/ToggleComponent'
-import UpdateEffectComponent from './useUpdateEffect/UpdateEffectComponent'
 import AsyncComponent from './useAsync/AsyncComponent'
+import UpdateEffectComponent from './useUpdateEffect/UpdateEffectComponent'
+import TimeoutComponent from './useTimeout/TimeoutComponent'
+import ToggleComponent from './useToggle/ToggleComponent'
 
 function App() {
   // return <ToggleComponent />
   return <UpdateEffectComponent />
   // return <AsyncComponent />
+  // return <TimeoutComponent />
 }
 
 export default App
