9a0ac1986749cc599f4a08d86cf05384129af1cd
Oguz <oguz.oeztuerk@outlook.de>
2022-05-22 10:16:38 +0200

    add useDebounce custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import AsyncComponent from './useAsync/AsyncComponent'
+import DebounceComponent from './useDebounce/DebounceComponent'
 import UpdateEffectComponent from './useUpdateEffect/UpdateEffectComponent'
 import TimeoutComponent from './useTimeout/TimeoutComponent'
 import ToggleComponent from './useToggle/ToggleComponent'
 
 function App() {
-  // return <ToggleComponent />
-  return <UpdateEffectComponent />
   // return <AsyncComponent />
+  // return <DebounceComponent />
+  return <UpdateEffectComponent />
   // return <TimeoutComponent />
+  // return <ToggleComponent />
 }
 
 export default App
